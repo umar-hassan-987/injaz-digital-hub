1aee5df30c26dccd9359f76e01982fcffb14ed4a
muhammad-shahzab <mshahzaib0003@gmail.com>
2026-06-17 17:14:35 +0500

    feat: configure trailing slashes for static export and add Apache rewrite rules for routing support

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   transpilePackages: ['next-intl']
 };
 
